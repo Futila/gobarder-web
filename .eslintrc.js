f8e7827ff28dca1d648a98abb34aa716c6105b97
Fernando Futila <fernando.futila@gmail.com>
2022-04-18 21:27:13 +0100

    validações nos formulários de SignIn e SignUp usando yup

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
     'react/jsx-one-expression-per-line': 'off',
     'global-require': 'off',
+    'react/jsx-no-bind': 'off',
     'react-native/no-raw-text': 'off',
     'no-param-reassign': 'off',
     'no-underscore-dangle': 'off',
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
-import history from './services/history';
+import { BrowserRouter } from 'react-router-dom';
+// import history from './services/history';
 
 import './config/ReactotronConfig';
 
@@ -9,10 +9,10 @@ import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <Router history={history}>
+    <BrowserRouter>
       <MainRoutes />
       <GlobalStyle />
-    </Router>
+    </BrowserRouter>
   );
 }
 
