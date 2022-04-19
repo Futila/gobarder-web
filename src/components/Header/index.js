e468e7cf6a7f382e36d2c043e5d9830f76bcea4a
Fernando Futila <fernando.futila@gmail.com>
2022-04-20 00:46:58 +0100

    feat:configurando Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/logo-color.SVG';
+import { Container, Content, Profile } from './styles';
+
+export default function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarder" />
+          <Link to="/dashboard">DASHBOARD</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Diego Fernandes</strong>
+              <Link to="/profile">Meu perfil</Link>
+            </div>
+            <img
+              src="https://cdn.pixabay.com/photo/2016/04/01/10/11/avatar-1299805_960_720.png"
+              alt="Diego Fernandes"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
