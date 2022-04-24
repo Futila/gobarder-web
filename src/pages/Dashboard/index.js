2a7720edec35d8bef96794189b1131be090e116c
Fernando Futila <fernando.futila@gmail.com>
2022-04-24 19:44:42 +0100

    feat: Estilização do dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,5 +1,41 @@
 import React from 'react';
+// import api from '~/services/api';
+
+import { MdChevronLeft, MdChevronRight } from 'react-icons/md';
+
+import { Container, Time } from './styles';
 
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Container>
+      <header>
+        <button type="button">
+          <MdChevronLeft size={36} color="#FFF" />
+        </button>
+        <strong>31 de Maio</strong>
+        <button type="button">
+          <MdChevronRight size={36} color="#FFF" />
+        </button>
+      </header>
+
+      <ul>
+        <Time past>
+          <strong>08:00</strong>
+          <span>Fernando Futila</span>
+        </Time>
+        <Time available>
+          <strong>09:00</strong>
+          <span>Em aberto</span>
+        </Time>
+        <Time>
+          <strong>10:00</strong>
+          <span>Fernando Futila</span>
+        </Time>
+        <Time>
+          <strong>11:00</strong>
+          <span>Fernando Futila</span>
+        </Time>
+      </ul>
+    </Container>
+  );
 }
--- src/pages/_layouts/default/styles.js
+++ src/pages/_layouts/default/styles.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  height: auto;
+  height: 100%;
   background: linear-gradient(-90deg, #7159c1, #ab59c1);
 `;
